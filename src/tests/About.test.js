5c7f125428cebe23217e8f8aa66ca5b75ec4b21c
Thierry <varelathierry@gmail.com>
2022-09-19 17:07:31 -0300

    req 2 tests componente about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+test('testes componente About.js', () => {
+  renderWithRouter(<About />);
+
+  const aboutTitle = screen.getByRole('heading', { name: /about pokédex/i });
+  const image = screen.getByRole('img', { name: /pokédex/i });
+
+  expect(aboutTitle).toBeInTheDocument();
+  expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
