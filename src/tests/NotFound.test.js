638e6fcc244be2e5b4b10a2fc6b740c80aebad26
Thierry <varelathierry@gmail.com>
2022-09-19 18:35:52 -0300

    req 4 tests componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+test('testes componente About.js', () => {
+  renderWithRouter(<NotFound />);
+  const title = screen.getByRole('heading', {
+    name: /page requested not found/i,
+  });
+  const image = screen
+    .getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    );
+  expect(title).toBeInTheDocument();
+  expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
