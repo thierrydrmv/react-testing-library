2924d4b849ccb7ff5122cb6354676aaf8941acdf
Thierry <varelathierry@gmail.com>
2022-09-20 18:52:55 -0300

    refatorando teste favoritepokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,17 +4,24 @@ import FavoritePokemons from '../pages/FavoritePokemons';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-test('testes componente FavoritePokemons.js', () => {
-  renderWithRouter(<FavoritePokemons />);
-  const noFavoritesText = screen.getByText('No favorite pokemon found');
-  expect(noFavoritesText).toBeInTheDocument();
-  renderWithRouter(<App />);
-  const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
-  userEvent.click(linkMoreDetails);
-  const checkBoxFavorite = screen.getByLabelText(/pokémon favoritado?/i);
-  userEvent.click(checkBoxFavorite);
-  const linkToFavorites = screen.getByRole('link', { name: /favorite pokémons/i });
-  userEvent.click(linkToFavorites);
-  const numberOfFavorites = screen.getAllByText(/more details/i);
-  expect(numberOfFavorites).toHaveLength(1);
+describe('testes componente FavoritePokemons.js', () => {
+  it(
+    'testa a mensagem: No favorite pokemon found caso não exista pokémons favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      const noFavoritesText = screen.getByText('No favorite pokemon found');
+      expect(noFavoritesText).toBeInTheDocument();
+    },
+  );
+  it('testa se ao favoritar um pokemon ele é renderizado', () => {
+    renderWithRouter(<App />);
+    const linkMoreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(linkMoreDetails);
+    const checkBoxFavorite = screen.getByLabelText(/pokémon favoritado?/i);
+    userEvent.click(checkBoxFavorite);
+    const linkToFavorites = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(linkToFavorites);
+    const numberOfFavorites = screen.getAllByText(/more details/i);
+    expect(numberOfFavorites).toHaveLength(1);
+  });
 });
