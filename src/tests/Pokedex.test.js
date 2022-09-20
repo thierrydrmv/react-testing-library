9cbd46c6a9f44c66e14f33c95f8627b2a0f17534
Thierry <varelathierry@gmail.com>
2022-09-20 19:55:37 -0300

    refatorando teste pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,7 +4,7 @@ import Pokedex from '../pages/Pokedex';
 import renderWithRouter from '../renderWithRouter';
 import data from '../data';
 
-test('testes componentes Pokedex.js', () => {
+describe('testes componentes Pokedex.js', () => {
   const obj = {
     4: false,
     10: false,
@@ -16,26 +16,67 @@ test('testes componentes Pokedex.js', () => {
     148: false,
     151: false,
   };
+  it('testa se a página contem um heading h2 com o texo encountered pokémons', () => {
+    renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
+    const title = screen
+      .getByRole('heading', { name: /encountered pokémons/i, level: 2 });
+    expect(title).toBeInTheDocument();
+  });
+  it(
+    'testa se é exibido o próximo pokemon da lista quando clica no botão próximo pokemon',
+    () => {
+      renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
+      const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+      expect(btnNextPokemon).toBeInTheDocument();
+      const pikachu = screen.getByText(/pikachu/i);
+      expect(pikachu).toBeInTheDocument();
+      userEvent.click(btnNextPokemon);
+      const charmander = screen.getByText(/charmander/i);
+      expect(charmander).toBeInTheDocument();
+    },
+  );
+  it(
+    'testa se a lista recomeça ao clicar no botão estando no último pokemon.',
+    () => {
+      renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
+      const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      userEvent.click(btnNextPokemon);
+      const dragonair = screen.getByText(/dragonair/i);
+      expect(dragonair).toBeInTheDocument();
+      userEvent.click(btnNextPokemon);
+      const pikachu = screen.getByText(/pikachu/i);
+      expect(pikachu).toBeInTheDocument();
+    },
+  );
+  it('testa se aparece apenas um pokemon por vez', () => {
+    renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
+    const pokemons = screen.getAllByRole('link', { name: /more details/i });
+    expect(pokemons).toHaveLength(1);
+  });
+  it('testa se a pokedex tem 7 botões de filtro diferentes e o botão sem filtro', () => {
+    renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
+    const btnAll = screen.getAllByRole('button');
+    expect(btnAll[0]).toBeInTheDocument();
+    const bntFilter = screen.getAllByTestId('pokemon-type-button');
+    const bntSize = 7;
+    const filterTypes = [...new Set(bntFilter)];
+    expect(filterTypes).toHaveLength(bntSize);
 
-  renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
-  const title = screen.getByRole('heading', { name: /encountered pokémons/i });
-  expect(title).toBeInTheDocument();
-
-  const btnNextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
-
-  expect(btnNextPokemon).toBeInTheDocument();
-  const btnAll = screen.getAllByRole('button');
-
-  userEvent.click(btnAll[0]);
-  expect(btnAll[0]).not.toHaveAttribute('disabled');
-
-  const bntFilter = screen.getAllByTestId('pokemon-type-button');
-  const bntSize = 7;
-  const filterTypes = [...new Set(bntFilter)];
-  expect(filterTypes).toHaveLength(bntSize);
-
-  const poisonPokemon = screen.getByRole('button', { name: /poison/i });
-  userEvent.click(poisonPokemon);
-  const namePokemon = screen.getByText(/ekans/i);
-  expect(namePokemon).toBeInTheDocument();
+    userEvent.click(btnAll[0]);
+    expect(btnAll[0]).not.toHaveAttribute('disabled');
+  });
+  it('testa se ao clicar no filtro type apenas um tipo de pokemon aparece', () => {
+    renderWithRouter(<Pokedex isPokemonFavoriteById={ obj } pokemons={ data } />);
+    const poisonPokemon = screen.getByRole('button', { name: /poison/i });
+    userEvent.click(poisonPokemon);
+    const typePokemon = screen.getByTestId('pokemon-type');
+    expect(typePokemon.innerHTML).toBe('Poison');
+  });
 });
